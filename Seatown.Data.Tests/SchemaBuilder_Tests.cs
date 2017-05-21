dc38463a374f89eb16f1a20f611a0ae04534167f
KevinWiener <twenty1918@hotmail.com>
2017-05-21 01:06:22 -0500

    Add files via upload

---
+++ Seatown.Data.Tests/SchemaBuilder_Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Seatown.Data.Tests
+{
+    [TestClass]
+    public class SchemaBuilder_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var db = new Seatown.Data.Schemas.Database();
+
+        }
+    }
+}
